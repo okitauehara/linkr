1f0f43420886176c41da54d6ebd1c1da8beca479
Kadugs <cadu20.gomes@gmail.com>
2021-09-16 10:36:41 -0300

    main corrigidaa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/Header'
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<Header />
 			<Switch>
 				<Route>
 					<h1>Hello World!</h1>
