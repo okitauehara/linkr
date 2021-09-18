2a9826661b91b96d21f55f971002f6e1cd12b5df
Kadugs <cadu20.gomes@gmail.com>
2021-09-18 15:59:11 -0300

    tooltip implementado

--- src/components/pages/userposts/UserPosts.js
+++ src/components/pages/userposts/UserPosts.js
@@ -1,32 +1,44 @@
-import mockedPosts from './mockedPosts'
 import UserPost from '../../shared/UserPost'
 import { useParams } from 'react-router'
+import Swal from "sweetalert2";
+import Loading from "../../shared/Loading";
 import ContainerStyle from "../../shared/ContainerStyle"
 import { useContext, useEffect, useState } from 'react';
 import { getUserPosts } from '../../../service/API';
 import UserContext from '../../../contexts/UserContext';
 
 export default function UserPosts() {
-
-    const userId = useParams();
+    const userId = useParams().id;
     const { user } = useContext(UserContext);
-    const [userPosts, setUserPosts] = useState([]);
+    const [userPosts, setUserPosts] = useState('');
 
     useEffect(() => {
-        getUserPosts(user.token, userId)
-            .then((r) => setUserPosts(r.data))
-            .catch(() => console.error);
-    }, [userId, setUserPosts, user.token])
-
-    console.log(userPosts);
-
+        getUserPosts({token: user.token, userId: userId})
+            .then((res) => {
+                console.log(res)
+                setUserPosts(res.data)
+            })
+            .catch(() => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Houve uma falha ao obter os posts, por favor atualize a página"
+                })
+            });
+    }, [userId, user.token]);
+  
+    if (!userPosts) {
+        return <Loading />
+    } else if(userPosts.posts.length === 0) {
+        return <p><br/><br/><br/><br/><br/><br/>Nenhum post encontrado</p>;
+    }
     return (
         <ContainerStyle>
             <div className="user-header">
-                <img src={mockedPosts.user.avatar} alt='' />
-                <h1> {mockedPosts.user.username}'s posts </h1>
+                <img src={userPosts.posts.avatar} alt='' />
+                <h1> {userPosts.posts.username}'s posts </h1>
             </div>
-            {mockedPosts.posts.map((post, index) => (
+            {userPosts.posts.map((post, index) => (
                 <UserPost post={post} key={index}/>
             ))}
         </ContainerStyle>
--- src/components/shared/UserPost.js
+++ src/components/shared/UserPost.js
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { AiFillHeart } from 'react-icons/ai'
 import ContainerUserPost from './ContainerUserPost'
 import styled from 'styled-components';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { toggleLike } from '../../service/API';
 import UserContext from '../../contexts/UserContext';
+import ReactTooltip from 'react-tooltip';
 
 export default function UserPost(props) {
     const {
@@ -19,11 +20,10 @@ export default function UserPost(props) {
     } = props.post;
     const { userInfo } = props;
 
-    const { user } = useContext(UserContext);
-
-    const [liked, setLiked] = useState(likes.some(like => like.userId === user.user.id));
+    const { user: thisUser } = useContext(UserContext);
+    const [tooltipMessage, setTooltipMessage] = useState('')
+    const [liked, setLiked] = useState(likes.some(like => like.userId === thisUser.user.id));
     const [postLikes, setPostLikes] = useState(likes.length);
-
     function checkHashtag() {
         const textCheck = text.split(' ').map((word, index) => {
             if (word[0] === '#') {
@@ -36,24 +36,80 @@ export default function UserPost(props) {
         return textCheck;
     }
 
+    useEffect(() => {
+
+        switch (likes.length) {
+            case 0: 
+                return;
+            case 1: 
+                setTooltipMessage(`${liked ? 'Você' : likes[0]["user.username"]} curtiu`);
+                break;
+            case 2:
+                setTooltipMessage(
+                    `${liked ? 'Você e ' + likes[0]["user.username"] 
+                    : 
+                    likes[0]["user.username"] + ' e ' + likes[1]["user.username"] 
+                    } curtiram`
+                );
+                break;
+            default:
+                setTooltipMessage(
+                    `${liked ? 'Você, ' + likes[0]["user.username"] 
+                    : 
+                    likes[0]["user.username"] + ', ' + likes[1]["user.username"] 
+                    } e outras ${likes.length - 2} pessoas`
+                );   
+            }
+
+        if (likes.length === 0) {
+            return;
+        }
+        if(likes.length === 1) {
+            
+        } 
+        else if(liked && likes.length > 3) {
+            console.log(likes)
+            setTooltipMessage(`Você, ${likes[0]["user.username"]} e outras ${likes.length - 2} pessoas`)
+        } else if (!liked && likes.length > 3) {
+            setTooltipMessage(`${likes[0]["user.username"]}, ${likes[1]["user.username"]} e outras ${likes.length - 2} pessoas`)
+        }
+
+    }, [tooltipMessage, setTooltipMessage, likes, liked])
+
     function changeLike() {
         if(!liked) {
             setLiked(true);
             setPostLikes(postLikes + 1);
-            toggleLike({ token: user.token, postId: id, status: 'like' })
+            toggleLike({ token: thisUser.token, postId: id, statthisUs: 'like' })
         } else {
             setLiked(false);
             setPostLikes(postLikes - 1);
-            toggleLike({ token: user.token, postId: id, status: 'dislike' })
+            toggleLike({ token: thisUser.token, postId: id, status: 'dislike' })
         }
     }
-
     return (
         <ContainerUserPost>
             <div className="photo-and-likes">
-                <Link to={`/user/${id}`}><img src={userInfo.avatar} alt=''/></Link> 
-                {liked ? <AiFillHeart style={{color: '#ac0000'}} onClick={changeLike} /> : <AiOutlineHeart onClick={changeLike} />}
-                <p>{postLikes} {postLikes <= 1 ? 'like' : 'likes'}</p>
+                <Link to={`/user/${id}`}><img src={userInfo.avatar} alt=''/></Link>
+                <Likes data-tip={tooltipMessage} >
+                    {liked ? 
+                        <AiFillHeart
+                        style={{color: '#ac0000'}} 
+                        onClick={changeLike} 
+                        /> 
+                        : 
+                        <AiOutlineHeart 
+                        onClick={changeLike}
+                        />
+                    }
+                    <p>{postLikes} {postLikes <= 1 ? 'like' : 'likes'}</p>
+                    <ReactTooltip 
+                        place="bottom"
+                        type="light"
+                        backgroundColor="#FFFFFF30"
+                        textColor="#505050"
+                    />
+                </Likes>
             </div>
             <div className="main-post">
                 <Link to={`/user/${id}`}><p><strong>{userInfo.username}</strong></p></Link>
@@ -74,4 +130,10 @@ export default function UserPost(props) {
 const HashtagCSS = styled.span`
     font-weight: 700;
     color: #ffffff;
+`;
+const Likes = styled.div `
+    cursor: pointer;  
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
