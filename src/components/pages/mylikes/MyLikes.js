7946423735c790368d879256685064580168e97f
pedrohotz <pedrohotz2002@gmail.com>
2021-09-18 11:21:18 -0300

    Tela mylikes implementada

--- src/components/pages/mylikes/MyLikes.js
+++ src/components/pages/mylikes/MyLikes.js
@@ -1,69 +1,58 @@
 
 import { useContext, useEffect,useState } from "react"
-import { useHistory } from "react-router";
 import UserContext from "../../../contexts/UserContext"
 import { getMylikes } from "../../../service/API";
-import UserPost from "../userposts/UserPost";
+import UserPost from '../../shared/UserPost'
 import styled from "styled-components";
-import mockedPosts from "../userposts/mockedPosts";
+import ContainerStyle from "../../shared/ContainerStyle";
+import Trending from "../../shared/Trending";
+import Swal from "sweetalert2";
 export default function MyLikes() {
     const {user} = useContext(UserContext);
     const [posts,setPosts] = useState([]);
-    let history = useHistory();
+   
     useEffect(()=>{
         getMylikes(user.token).then((res)=> {
             setPosts(res.data)
         }).catch(Erro);
+         // eslint-disable-next-line
     },[])
 
     function Erro(){
-        console.log("deu ruim");
-        //history.push("/");
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Houve uma falha ao obter os posts, por favor atualize a página"
+        })
     }
 
-
+    console.log(posts);
     return (
-        <ContainerUserPosts>
-            <div className="user-header">
-                <h1>my likes</h1>
-            </div>
-            {mockedPosts.posts.map((post, index) => (
-                <UserPost post={post} key={index}/>
-            ))}
-        </ContainerUserPosts>
+        <PageContainer>
+        <ContainerStyle>
+        <div className="user-header">
+            <h1>my likes</h1>
+        </div>
+        {posts.length === 0 ?
+			<p style={{
+				fontSize: '25px',
+				color: '#ffffff',
+				marginTop: '30px'}}>
+			Nenhum post encontrado
+			</p>:
+			posts.posts.map((post, index) => (
+            	<UserPost post={post} key={index}/>
+        ))}
+        </ContainerStyle>
+        <Trending />
+        </PageContainer>
     )
 }
 
-
-const ContainerUserPosts = styled.div`
-    margin-top: 132px;
+const PageContainer = styled.div`
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 20px;
-
-    .user-header { 
-        width: 611px;
-        padding-left: 18px;
-        display: flex;
-        align-items: center;
-        color: white;
-        font-family: 'Oswald', sans-serif;
-        font-size: 43px;
-    }   
-    .user-header img {
-        object-fit: cover;
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        margin-right: 18px;
-    }
-    @media (max-width: 620px) {
-        margin-top: 50px;
-
-        .user-header {
-            width: 100vw;
-            font-size: 33px;
-        }
-    }
+    flex-direction: row;
+    justify-content: center;
 `;
+
+
